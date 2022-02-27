df22e1bcb1c5b118cf15e6942dd3caf6e52da0ac
Gaurav <gaurav@glideinvest.com>
2022-02-27 22:16:44 +0530

    Added create user and token endpoint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,4 @@
 import time
-from django import db
-
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,4 +1,3 @@
-from ast import arg
 from django.test import Client, TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
